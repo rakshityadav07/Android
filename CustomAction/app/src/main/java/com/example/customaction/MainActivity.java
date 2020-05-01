31a4e41bcc7cf153d39247b8eb4709d9aeb9c2fe
rakshit yadav <rakshit20141970@gmail.com>
2020-05-01 12:28:52 +0530

    intent Action

---
+++ CustomAction/app/src/main/java/com/example/customaction/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.customaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setAction("com.example.customaction.NewActivity");
+                startActivity(i);
+            }
+        });
+
+    }
+}
