33ca82942e7076929a910f4289b0e3a12cc5a915
rakshit yadav <rakshit20141970@gmail.com>
2020-05-27 12:45:25 +0530

    SwipeUp with Progress Bar
    Signed-off-by: rakshit yadav <rakshit20141970@gmail.com>

--- NewsApp/app/src/main/java/com/example/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
-import org.ocpsoft.prettytime.PrettyTime
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.lang.Exception
-import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -22,32 +20,43 @@ class MainActivity : AppCompatActivity() {
     val API_KEY = "984501863f994d0b9e62c20e49a8ffc7"
     val category = "business"
     var article = ArrayList<Article>()
+    lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        swipeRefreshLayout = findViewById(R.id.swipeRefresh)
+
         rvView.layoutManager = LinearLayoutManager(this)
-        val country = getCountry()
-        if (country != null) {
-            retreiveJson(country,API_KEY)
+        val country = getCountry() ?: "In"
+
+        swipeRefreshLayout.setOnRefreshListener {
+            retreiveJson(country, API_KEY)
         }
+
+        retreiveJson(country,API_KEY)
     }
+
+
 
     fun retreiveJson(country : String,apiKey : String){
 
+        swipeRefreshLayout.isRefreshing = true;
         var call: Call<Headline> = ApiClient.getInstance().api.getHeadlines(country, apiKey)
             call.enqueue(object : Callback<Headline>{
                 override fun onFailure(call: Call<Headline>, t: Throwable) {
-                    Toast.makeText(baseContext, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show()
+                    swipeRefreshLayout.isRefreshing = false;
+                    Toast.makeText(baseContext, t.localizedMessage, Toast.LENGTH_SHORT).show()
                 }
 
                 override fun onResponse(call: Call<Headline>, response: Response<Headline>) {
                     if(response.isSuccessful && response.body()?.articles != null){
+                        swipeRefreshLayout.isRefreshing = false;
                         article.clear()
                         article = response.body()?.articles as ArrayList<Article>
                         val adapter = Adapter(article)
-                        rvView.setAdapter(adapter)
+                        rvView.adapter = adapter
                     }
                 }
             })
@@ -61,3 +70,14 @@ class MainActivity : AppCompatActivity() {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
